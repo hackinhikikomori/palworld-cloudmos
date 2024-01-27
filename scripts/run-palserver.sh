de6d6de25dcb119237171619e008c3c805c444e5
hackinhikikomori <120437017+hackinhikikomori@users.noreply.github.com>
2024-01-27 17:15:38 -0600

    Create run-palserver.sh

---
+++ scripts/run-palserver.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Link SteamCMD Files to Steam User
+ mkdir -p $HOME/.steam \
+ && ln -s $HOME/.local/share/Steam/steamcmd/linux32 $HOME/.steam/sdk32 \
+ && ln -s $HOME/.local/share/Steam/steamcmd/linux64 $HOME/.steam/sdk64 \
+ && ln -s $HOME/.steam/sdk32/steamclient.so $HOME/.steam/sdk32/steamservice.so \
+ && ln -s $HOME/.steam/sdk64/steamclient.so $HOME/.steam/sdk64/steamservice.so
+
+source /home/steam/scripts/utils.sh
+
+palworld_launch
